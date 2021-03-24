e6cada96afa0f042f82a96ca560f31b444bbc986
Miguel Toscano <miguel.toscano@naranjax.com>
2021-03-24 15:43:35 -0300

    removed comments

--- src/infrastructure/mongoDb.ts
+++ src/infrastructure/mongoDb.ts
@@ -1,25 +1,3 @@
-// import TripRepository from '../core/repositories/trip.repository';
-// import Trip from '../core/entities/Trip';
-// import MongoClient from 'mongodb';
-
-// class TripMongo implements TripRepository {
-//   public async getById(id: string): Promise<Trip> {
-//     const collection = await this.getCollection();
-//     const trip: Trip = await collection.findOne({ id });
-//     return trip;
-//   }
-
-//   /// COllection per request
-//   private async getCollection() {
-//     const url = 'mongodb://localhost:27017';
-//     const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-
-//     const db = client.db('ticketing');
-//     return db.collection('trips');
-//   }
-// }
-// export default TripMongo;
-
 import MongoClient from 'mongodb';
 import ApiKeysRepository from '../core/apiKeys/ApiKeysRepository';
 import ApiKeyDTO from '../core/apiKeys/ApiKeyDTO';
